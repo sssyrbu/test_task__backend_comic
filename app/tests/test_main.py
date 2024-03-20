60e3ff52315c2f43bf2d201a62c7f09084b45f95
Roman Syrbu <syrburoman0@gmail.com>
2024-03-20 10:44:53 +0300

    finished unit tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,7 +1,25 @@
 from fastapi.testclient import TestClient
 from main import app, get_db
 from .testdb import Base, engine, TestingSessionLocal
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 
+
+class Comic(Base):
+    __tablename__ = "comics"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    title = Column(String, nullable=True)
+    author = Column(String, nullable=True)
+    rating = Column(Float)
+
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    comic_id = Column(Integer, ForeignKey("comics.id"))
+    user_id = Column(Integer)
+    value = Column(Float)
 
 Base.metadata.create_all(bind=engine)
 
@@ -20,7 +38,10 @@ app.dependency_overrides[get_db] = override_get_db
 
 
 def test_rate_comic():
-    response = client.post("/api/ratings", json={"comic_id": 1, "user_id": 1, "value": 5})
+    response = client.post(
+        "/api/ratings",
+        params={"comic_id": 1, "user_id": 1, "value": 5}
+    )
     assert response.status_code == 200
     data = response.json()
     assert data["message"] == "Вы оценили комикс"
@@ -29,15 +50,18 @@ def test_rate_comic():
     assert data["value"] == 5
 
 
-# def test_get_comic_rating():
-#     comic_id = 2
-#     # imitate a comic rating
-#     client.post("/api/ratings", json={"comic_id": comic_id, "user_id": 2, "value": 4.5})
+def test_get_comic_rating():
+    comic_id = 2
+    # imitate a comic rating
+    client.post(
+        "/api/ratings",
+        params={"comic_id": comic_id, "user_id": 1, "value": 5}
+    )
 
-#     response = client.get(f"/api/comics/{comic_id}/rating/")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "comic_rating" in data
+    response = client.get(f"/api/comics/{comic_id}/rating/")
+    assert response.status_code == 200
+    data = response.json()
+    assert "comic_rating" in data
 
 
 def test_root():
